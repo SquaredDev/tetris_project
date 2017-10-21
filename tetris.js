ffa4218e595c41e508718e3a102a8cc0be2f632b
James <boydston.j@gmail.com>
2017-10-21 13:23:14 -0700

    keyboard input added

--- tetris.js
+++ tetris.js
@@ -29,11 +29,20 @@ function drawMatrix(matrix , offset) {
   })
 }
 
+let dropCounter = 0;
+let dropInterval = 1000;
+
 let lastTime = 0
 function update(time = 0) {
   const deltaTime = time - lastTime;
   lastTime = time;
-  console.log(deltaTime);
+
+  dropCounter += deltaTime;
+  if (dropCounter > dropInterval) {
+    player.pos.y++;
+    dropCounter = 0;
+  }
+
   draw();
   requestAnimationFrame(update);
 };
@@ -44,5 +53,19 @@ const player = {
   pos: {x : 5, y : 5},
   matrix: matrix
 }
+
+document.addEventListener('keydown', event => {
+  console.log(event);
+  if (event.keyCode === 37) {
+    player.pos.x--;
+  }
+  else if (event.keyCode === 39) {
+    player.pos.x++;
+  }
+  else if (event.keyCode === 40) {
+    player.pos.y++;
+    dropCounter = 0;
+  }
+})
 
 update();

910f8aa44261ae63f0e9a92520a8fe4d5fe0c5b1
James <boydston.j@gmail.com>
2017-10-21 13:58:24 -0700

    implemented collision function, dont quite understand it yet

--- tetris.js
+++ tetris.js
@@ -3,13 +3,33 @@ const context = canvas.getContext('2d');
 
 context.scale(20, 20);
 
-
-
 const matrix = [
   [0,0,0],
   [1,1,1],
   [0,1,0],
 ];
+
+function collide(arena, player) {
+  const [m, o] = [player.matrix, player.pos];
+  for (let y = 0; y < m.length; y++) {
+    for (var x = 0; x < m[y].length; ++x) {
+      if (m[y][x]  !== 0 &&
+        (arena[y + o.y] &&
+        arena[y + o.y][x + o.x]) !== 0) {
+          return true;
+        }
+    }
+  }
+  return false;
+}
+
+function createMatrix(w, h) {
+  const matrix = [];
+  while (h--) {
+    matrix.push(new Array(w).fill(0));
+  }
+  return matrix;
+}
 
 function draw() {
   context.fillStyle = '#000';
@@ -29,6 +49,27 @@ function drawMatrix(matrix , offset) {
   })
 }
 
+function merge(arena, player) {
+  player.matrix.forEach((row, y) => {
+    row.forEach((value, x) =>{
+      if (value !==0){
+        arena[y + player.pos.y][x + player.pos.x] = value;
+      }
+    });
+  });
+}
+
+
+function playerDrop(){
+  player.pos.y++;
+  if (collide(arena, player)) {
+    player.pos.y--;
+    merge(arena, player);
+    player.pos.y = 0;
+  }
+  dropCounter = 0;
+}
+
 let dropCounter = 0;
 let dropInterval = 1000;
 
@@ -39,15 +80,14 @@ function update(time = 0) {
 
   dropCounter += deltaTime;
   if (dropCounter > dropInterval) {
-    player.pos.y++;
-    dropCounter = 0;
+    playerDrop();
   }
 
   draw();
   requestAnimationFrame(update);
 };
 
-
+const arena = createMatrix(12, 20);
 
 const player = {
   pos: {x : 5, y : 5},
@@ -55,7 +95,6 @@ const player = {
 }
 
 document.addEventListener('keydown', event => {
-  console.log(event);
   if (event.keyCode === 37) {
     player.pos.x--;
   }
@@ -63,8 +102,7 @@ document.addEventListener('keydown', event => {
     player.pos.x++;
   }
   else if (event.keyCode === 40) {
-    player.pos.y++;
-    dropCounter = 0;
+    playerDrop();
   }
 })
 
